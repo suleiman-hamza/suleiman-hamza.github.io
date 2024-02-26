0467da07880d36e62573b86fca41fd798a090ca0
Suleiman Hamza <88393968+suleiman-hamza@users.noreply.github.com>
2024-02-26 09:15:04 +0000

    add intersection observer api

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
+// instersection observer API
+const target = document.querySelector('.target')
+const proper = document.querySelector('.proper')
+const player = document.querySelector('.player')
+const touch = document.querySelector('.touch')
+
+
+let option = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.2
+}
+
+let collect = (entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animate')
+    }
+  })
+}
+let observer = new IntersectionObserver(collect, option)
+
+observer.observe(target)
+observer.observe(proper)
+observer.observe(player)
+observer.observe(touch)
+
+
 //scroll to top button functionality
 let myBtn = document.getElementById("scroll-to-top");
 
