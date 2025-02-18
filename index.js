0702839e8faacb3add54499f89518d73fa50dec4
suleiman hamza <hamzasuleiman530@gmail.com>
2025-02-18 02:26:10 +0100

    experience section

--- index.js
+++ index.js
@@ -27,19 +27,22 @@ observer.observe(touch)
 
 
 //scroll to top button functionality
-let myBtn = document.getElementById("scroll-to-top");
+document.addEventListener("DOMContentLoaded", function() {
+  //scroll to top button functionality
+  let myBtn = document.getElementById("scroll-to-top");
 
-window.onscroll = function() {scrollFunction()};
+  window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    myBtn.style.display = "block";
-  } else {
-    myBtn.style.display = "none";
+  function scrollFunction() {
+      if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+          myBtn.style.display = "block";
+      } else {
+          myBtn.style.display = "none";
+      }
   }
-};
 
-myBtn.addEventListener('click', function() {
-    document.documentElement.scrollTop = 0;
-    console.log('clicked the button');
-})
+  myBtn.addEventListener('click', function() {
+      document.documentElement.scrollTop = 0;
+      console.log('clicked the button');
+  });
+});
